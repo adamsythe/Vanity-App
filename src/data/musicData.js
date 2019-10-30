feb8e3d56ef528f039f4087f8e3fed46e950761e
adamkatz <adam.katz@proversity.org>
2019-10-30 17:37:52 +0200

    uploaded music data

---
+++ src/data/musicData.js
@@ -0,0 +1,54 @@
+const music = [
+	{
+		title: 'Without You',
+		artist: 'Oh Wonder',
+		link: 'https://www.youtube.com/watch?v=zLAhRiUeJ8E&list=RDzLAhRiUeJ8E&index=1',
+	},
+	{
+		title: 'Bad Habit',
+		artist: 'The Kooks',
+		link: 'https://www.youtube.com/watch?v=3tUh-x-fp8Q&list=RDzLAhRiUeJ8E&index=4',
+	},
+	{
+		title: 'You & Me (Flume Remix)',
+		artist: 'Disclosure',
+		link: 'https://www.youtube.com/watch?v=_zPlr-o-YEQ&list=RD_zPlr-o-YEQ&start_radio=1',
+	},
+	{
+		title: 'Nothings Gonna Hurt You Baby',
+		artist: 'Cigarettes After Sex',
+		link: 'https://www.youtube.com/watch?v=R2LQdh42neg',
+	},
+	{
+		title: 'Eyes On Fire (Twilight Soundtrack Version)',
+		artist: 'Blue Foundation',
+		link: 'https://www.youtube.com/watch?v=YGAKiiUpvJ0',
+	},
+	{
+		title: 'No Diggity',
+		artist: 'Chet Faker',
+		link: 'https://www.youtube.com/watch?v=HIfFA8-RaHQ',
+	},
+	{
+		title: 'Genghis Khan',
+		artist: 'Mike Snow',
+		link: 'https://www.youtube.com/watch?v=P_SlAzsXa7E',
+	},
+	{
+		title: 'Make It Wit Chu (Album Version)',
+		artist: 'Queens Of The Stone Age',
+		link: 'https://www.youtube.com/watch?v=hg14Ocs03xA'
+	},
+	{
+		title: 'Anvil',
+		artist: 'Lorn',
+		link: 'https://www.youtube.com/watch?v=CqaAs_3azSs'
+	},
+	{
+		title: 'Crow (DJ-Kicks)',
+		artist: 'Forest Swords',
+		link: 'https://www.youtube.com/watch?v=lan-Pjv99Xk'
+	}
+]
+
+export { music }
